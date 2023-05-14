450c1ed968337e2123e0f5b1118a72945ab080bf
bytfr <2678509244@qq.com>
2023-05-14 19:16:53 +0800

    Update DefaultPreview.tsx

--- components/previews/DefaultPreview.tsx
+++ components/previews/DefaultPreview.tsx
@@ -44,7 +44,7 @@ const DefaultPreview: FC<{ file: OdFileObject }> = ({ file }) => {
                       Quick XOR
                     </td>
                     <td className="whitespace-nowrap py-1 px-3 font-mono text-gray-500 dark:text-gray-400">
-                      {file.file.hashes?.quickXorHash || 'Unavailable'}
+                      {file.file.hashes?.quickXorHash || '不可用'}
                     </td>
                   </tr>
                   <tr className="border-y bg-white dark:border-gray-700 dark:bg-gray-900">
@@ -52,7 +52,7 @@ const DefaultPreview: FC<{ file: OdFileObject }> = ({ file }) => {
                       SHA1
                     </td>
                     <td className="whitespace-nowrap py-1 px-3 font-mono text-gray-500 dark:text-gray-400">
-                      {file.file.hashes?.sha1Hash || 'Unavailable'}
+                      {file.file.hashes?.sha1Hash || '不可用'}
                     </td>
                   </tr>
                   <tr className="border-y bg-white dark:border-gray-700 dark:bg-gray-900">
@@ -60,7 +60,7 @@ const DefaultPreview: FC<{ file: OdFileObject }> = ({ file }) => {
                       SHA256
                     </td>
                     <td className="whitespace-nowrap py-1 px-3 font-mono text-gray-500 dark:text-gray-400">
-                      {file.file.hashes?.sha256Hash || 'Unavailable'}
+                      {file.file.hashes?.sha256Hash || '不可用'}
                     </td>
                   </tr>
                 </tbody>
